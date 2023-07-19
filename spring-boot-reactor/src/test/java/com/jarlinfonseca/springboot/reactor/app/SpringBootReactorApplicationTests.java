12d3789aad66e3027b20b6b3667fb0578e36f8a7
Jarlin Fonseca <jarlin.fonsecab@pragma.com.co>
2023-07-19 08:22:18 -0500

    First commit-Creando un Stream Flux Observable

---
+++ spring-boot-reactor/src/test/java/com/jarlinfonseca/springboot/reactor/app/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarlinfonseca.springboot.reactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
