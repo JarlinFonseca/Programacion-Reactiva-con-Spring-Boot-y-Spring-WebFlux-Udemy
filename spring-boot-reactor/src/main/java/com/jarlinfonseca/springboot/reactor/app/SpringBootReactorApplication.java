7b21c1f4ea4759d4da5fde7d160e861880609a50
Jarlin Fonseca <jarlin.fonsecab@pragma.com.co>
2023-07-21 13:55:00 -0500

    Intervalos infinito con interval y operador el delayElements

--- spring-boot-reactor/src/main/java/com/jarlinfonseca/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/jarlinfonseca/springboot/reactor/app/SpringBootReactorApplication.java
@@ -3,6 +3,7 @@ package com.jarlinfonseca.springboot.reactor.app;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,8 +30,26 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		ejemploDelayElements();
+		ejemploIntervalInfinito();
 		
+	}
+	
+	public void ejemploIntervalInfinito() throws InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		
+		Flux.interval(Duration.ofSeconds(1))
+		.doOnTerminate(latch::countDown)
+		.flatMap(i ->{
+			if(i>=5) {
+				return Flux.error(new InterruptedException("Solo hasta 5!"));
+			}
+			return Flux.just(i);
+		})
+		.map(i->"Hola "+i)
+		.retry(2)
+		.subscribe(s -> log.info(s), e -> log.error(e.getMessage()));
+		
+		latch.await();
 	}
 	
 	public void ejemploDelayElements() throws InterruptedException {
